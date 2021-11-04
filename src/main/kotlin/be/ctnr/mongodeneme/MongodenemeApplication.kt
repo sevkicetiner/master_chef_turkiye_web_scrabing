19250fdf4be1c74dcf09ad318973d6fed7084fe0
ctnr <sevki.ctnr@gmail.com>
2021-11-04 01:55:13 +0100

    dockerfile added

--- src/main/kotlin/be/ctnr/mongodeneme/MongodenemeApplication.kt
+++ src/main/kotlin/be/ctnr/mongodeneme/MongodenemeApplication.kt
@@ -1,6 +1,8 @@
 package be.ctnr.mongodeneme
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.ServletComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @ServletComponentScan
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration(exclude= [MongoAutoConfiguration::class])
 class MongodenemeApplication
 
 fun main(args: Array<String>) {
