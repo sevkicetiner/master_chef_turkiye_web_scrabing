1184f55a61efa74d1f8f2db260c5e919680a5502
Sevki Cetiner <sevki.ctnr@gmail.com>
2022-02-23 11:32:33 +0100

    cleaned

---
+++ target/classes/be/ctnr/mongodeneme/MongodenemeApplication.kt
@@ -0,0 +1,19 @@
+package be.ctnr.mongodeneme
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.servlet.DispatcherServlet
+
+@ServletComponentScan
+@EnableScheduling
+//@EnableAutoConfiguration(exclude= [MongoAutoConfiguration::class])
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+class MongodenemeApplication
+fun main(args: Array<String>) {
+	runApplication<MongodenemeApplication>(*args)
+}
